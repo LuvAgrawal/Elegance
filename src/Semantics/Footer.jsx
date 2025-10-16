0654e514c95697dac043289d229667be3a7c548e
Luv Agrawal <la24092002@gmail.com>
2025-10-16 11:32:25 +0530

    assets img added

--- src/Semantics/Footer.jsx
+++ src/Semantics/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <>
       <div className="bg-[#3f3f3f] text-white mt-5 p-5">
-        <div className="flex flex-col items-center align-middle justify-center">
+        <div className="flex flex-col
+         items-center align-middle justify-center">
           <h1>Pratika Weds Dinesh</h1>
           <h2>October 14, 2025</h2>
         </div>
